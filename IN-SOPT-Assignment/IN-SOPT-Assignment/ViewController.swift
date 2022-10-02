ac4a75619ba554bb1f4ced7eb192d6acc883f237
devxsby <devxsby@gmail.com>
2022-10-02 22:21:11 +0900

    [Add] 프로젝트 생성

---
+++ IN-SOPT-Assignment/IN-SOPT-Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IN-SOPT-Assignment
+//
+//  Created by devxsby on 2022/10/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
