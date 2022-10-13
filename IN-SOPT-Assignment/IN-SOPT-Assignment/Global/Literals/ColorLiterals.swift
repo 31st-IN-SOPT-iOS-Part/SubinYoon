6a7f3ce2b3942013164e4367613869be3b95450f
devxsby <devxsby@gmail.com>
2022-10-13 17:04:08 +0900

    [Add] 컬러 및 이미지 에셋 추가 및 Literals 추가

---
+++ IN-SOPT-Assignment/IN-SOPT-Assignment/Global/Literals/ColorLiterals.swift
@@ -0,0 +1,52 @@
+//
+//  ColorLiterals.swift
+//  IN-SOPT-Assignment
+//
+//  Created by devxsby on 2022/10/13.
+//
+
+import UIKit
+
+extension UIColor {
+    static var kakaoYellow: UIColor {
+        return UIColor(hex: "#FBE54C")
+    }
+    
+    static var kakaoGray: UIColor {
+        return UIColor(hex: "#F6F6F6")
+    }
+    
+    static var kakaoBackground: UIColor {
+        return UIColor(hex: "#879198")
+    }
+    
+    static var placeholerGray: UIColor {
+        return UIColor(hex: "#A09FA0")
+    }
+    
+    static var textGray: UIColor {
+        return UIColor(hex: "#7D7E7D")
+    }
+    
+    static var textDarkGray: UIColor {
+        return UIColor(hex: "#313030")
+    }
+}
+
+extension UIColor {
+    convenience init(hex: String, alpha: CGFloat = 1.0) {
+        var hexFormatted: String = hex.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
+        
+        if hexFormatted.hasPrefix("#") {
+            hexFormatted = String(hexFormatted.dropFirst())
+        }
+        
+        assert(hexFormatted.count == 6, "Invalid hex code used.")
+        var rgbValue: UInt64 = 0
+        Scanner(string: hexFormatted).scanHexInt64(&rgbValue)
+        
+        self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+                  green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+                  blue: CGFloat(rgbValue & 0x0000FF) / 255.0, alpha: alpha)
+    }
+}
