c86cd9ff8dcf9a3507165e6af9c443bad053d0ec
devxsby <devxsby@gmail.com>
2022-10-13 22:17:52 +0900

    [Chore] rootViewController 변경

--- IN-SOPT-Assignment/IN-SOPT-Assignment/Application/Coordinator/SceneDelegate.swift
+++ IN-SOPT-Assignment/IN-SOPT-Assignment/Application/Coordinator/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-                window.rootViewController = ViewController()
+                window.rootViewController = SignInViewController()
                 window.makeKeyAndVisible()
                 self.window = window
     }
