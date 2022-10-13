22f4de19680ef91275bb785627d39cde38df3bb2
devxsby <devxsby@gmail.com>
2022-10-13 23:46:41 +0900

    [Feat] AuthCompleteViewController 생성

--- IN-SOPT-Assignment/IN-SOPT-Assignment/Presentation/AuthCompleteScene/ViewController/AuthCompleteViewController.swift
+++ IN-SOPT-Assignment/IN-SOPT-Assignment/Presentation/AuthCompleteScene/ViewController/AuthCompleteViewController.swift
@@ -5,4 +5,59 @@
 //  Created by devxsby on 2022/10/13.
 //
 
-import Foundation
+import UIKit
+
+final class AuthCompleteViewController: UIViewController {
+    // MARK: - Properties
+    var viewModel: AuthCompleteViewModel!
+    
+    // MARK: - UI
+    private let welcomeLabel = UILabel().then {
+        $0.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
+        $0.textColor = .black
+        $0.numberOfLines = 0
+        $0.textAlignment = .center
+    }
+    
+    private lazy var completeButton = UIButton(type: .system).then {
+        $0.setAttributedTitle(NSAttributedString(string: "확인", attributes: [.font: UIFont.systemFont(ofSize: 15, weight: .medium), .foregroundColor: UIColor.black]), for: .normal)
+        $0.setTitleColor(.black, for: .normal)
+        $0.backgroundColor = .buttonYellow
+        $0.layer.cornerRadius = 10
+        $0.addTarget(self, action: #selector(completeButtonDidTap), for: .touchUpInside)
+    }
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+        setWelcomeLabel()
+    }
+    
+    // MARK: - Functions
+    private func configureUI() {
+        view.backgroundColor = .white
+        
+        view.addSubviews(welcomeLabel, completeButton)
+        
+        welcomeLabel.snp.makeConstraints { make in
+            make.centerX.equalTo(view.safeAreaLayoutGuide)
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(200)
+        }
+        
+        completeButton.snp.makeConstraints { make in
+            make.centerX.equalTo(view.safeAreaLayoutGuide)
+            make.top.equalTo(welcomeLabel.snp.bottom).offset(120)
+            make.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(20)
+            make.height.equalTo(45)
+        }
+    }
+    
+    private func setWelcomeLabel() {
+        welcomeLabel.text = viewModel.getUserEmail()
+    }
+    
+    @objc private func completeButtonDidTap() {
+        // 카톡 친구탭바 뷰로 전환
+    }
+}
