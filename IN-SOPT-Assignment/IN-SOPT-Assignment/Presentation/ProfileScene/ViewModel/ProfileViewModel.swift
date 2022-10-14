198703c8c55b7711efeb4f63948aab0d76af9151
devxsby <devxsby@gmail.com>
2022-10-14 10:52:37 +0900

    [Feat] ProfileViewModel 생성

--- IN-SOPT-Assignment/IN-SOPT-Assignment/Presentation/ProfileScene/ViewModel/ProfileViewModel.swift
+++ IN-SOPT-Assignment/IN-SOPT-Assignment/Presentation/ProfileScene/ViewModel/ProfileViewModel.swift
@@ -5,4 +5,29 @@
 //  Created by devxsby on 2022/10/14.
 //
 
-import Foundation
+import Combine
+
+final class ProfileViewModel: ViewModelType {
+    var userModel: UserModel
+
+    init(userModel: UserModel) {
+        self.userModel = userModel
+    }
+
+    struct Input {
+    }
+
+    struct Output {
+    }
+    
+    func getUserName() -> String {
+        return userModel.email
+    }
+}
+
+extension ProfileViewModel {
+    func transform(from input: Input) -> Output {
+        let output = Output()
+        return output
+    }
+}
