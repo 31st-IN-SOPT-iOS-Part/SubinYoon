c61aaee368399e6bcd7492c57a482cb40683b9b2
devxsby <devxsby@gmail.com>
2022-11-03 00:06:48 +0900

    [Feat] #5- Chat 뷰 추가

---
+++ IN-SOPT-Assignment/IN-SOPT-Assignment/Presentation/TabBar/ChatScene/ViewController/ChatViewController.swift
@@ -0,0 +1,128 @@
+//
+//  ChatViewController.swift
+//  IN-SOPT-Assignment
+//
+//  Created by devxsby on 2022/10/31.
+//
+
+import UIKit
+import Combine
+
+final class ChatViewController: UIViewController {
+    
+    // MARK: - Properties
+    var viewModel: ChatViewModel!
+    private var cancellable: Set<AnyCancellable> = []
+    
+    // MARK: - UI
+    private lazy var naviChatButton = UIButton(type: .system).then {
+        $0.setAttributedTitle(NSAttributedString(string: "채팅", attributes: [.font: UIFont.systemFont(ofSize: 22, weight: .medium), .foregroundColor: UIColor.black]), for: .normal)
+    }
+    
+    private lazy var naviOpenChatButton = UIButton(type: .system).then {
+        $0.setAttributedTitle(NSAttributedString(string: "오픈채팅", attributes: [.font: UIFont.systemFont(ofSize: 22, weight: .semibold), .foregroundColor: UIColor.subtitleGray]), for: .normal)
+    }
+    
+    private lazy var naviPlusButton = UIButton(type: .system).then {
+        $0.setImage(ImageLiterals.plusIcn, for: .normal)
+        $0.frame = CGRect(x: 0, y: 0, width: 21, height: 21)
+        $0.addTarget(self, action: #selector(plusButtonDidTap), for: .touchUpInside)
+    }
+    
+    private lazy var naviSettingButton = UIButton(type: .system).then {
+        $0.setImage(ImageLiterals.settingIcn, for: .normal)
+        $0.frame = CGRect(x: 0, y: 0, width: 21, height: 21)
+    }
+    
+    private lazy var chatListCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.isScrollEnabled = true
+        return collectionView
+    }()
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+        bind()
+        setDelegate()
+        registerCells()
+    }
+    
+    // MARK: - Functions
+    private func configureUI() {
+        navigationBarUI()
+        
+        view.backgroundColor = .white
+        
+        view.addSubview(chatListCollectionView)
+        
+        chatListCollectionView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    private func navigationBarUI() {
+        let spacer = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
+        spacer.width = 10
+        
+        self.navigationItem.leftBarButtonItems = [UIBarButtonItem(customView: naviChatButton), spacer, UIBarButtonItem(customView: naviOpenChatButton)]
+        self.navigationItem.rightBarButtonItems = [UIBarButtonItem(customView: naviPlusButton), spacer, UIBarButtonItem(customView: naviSettingButton)]
+    }
+    
+    private func bind() {
+    }
+    
+    private func setDelegate() {
+        chatListCollectionView.delegate = self
+        chatListCollectionView.dataSource = self
+    }
+    
+    private func registerCells() {
+        chatListCollectionView.register(ChatCollectionViewHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: ChatCollectionViewHeader.className)
+        chatListCollectionView.register(ChatCVC.self, forCellWithReuseIdentifier: ChatCVC.className)
+    }
+    
+    @objc func plusButtonDidTap() {
+        
+    }
+}
+
+// MARK: - UICollectionViewDelegate, UICollectionViewDataSource
+extension ChatViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.getChatCellCount()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ChatCVC.className, for: indexPath)
+                as? ChatCVC else { return UICollectionViewCell() }
+        cell.initCell(model: viewModel.chatModel[indexPath.row])
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewFlowLayout
+extension ChatViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let screenWidth = UIScreen.main.bounds.width
+        return CGSize(width: screenWidth, height: 55)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        switch kind {
+        case UICollectionView.elementKindSectionHeader:
+            let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: ChatCollectionViewHeader.className, for: indexPath)
+            return header
+        default:
+            return UICollectionReusableView()
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        let screenWidth = UIScreen.main.bounds.width
+        return CGSize(width: screenWidth, height: 80)
+    }
+}
---
+++ IN-SOPT-Assignment/IN-SOPT-Assignment/Presentation/TabBar/ChatScene/Views/ChatCVC.swift
@@ -0,0 +1,82 @@
+//
+//  ChatCVC.swift
+//  IN-SOPT-Assignment
+//
+//  Created by devxsby on 2022/11/02.
+//
+
+import UIKit
+
+final class ChatCVC: UICollectionViewCell {
+    
+    // MARK: - UI
+    private let profileImageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFit
+        $0.layer.masksToBounds = true
+    }
+    
+    private let userNameLabel = UILabel().then {
+        $0.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
+        $0.textColor = .black
+    }
+    
+    private let messageLabel = UILabel().then {
+        $0.font = UIFont.systemFont(ofSize: 11, weight: .regular)
+        $0.textColor = .subtitleGray
+    }
+    
+    private let timeLabel = UILabel().then {
+        $0.font = UIFont.systemFont(ofSize: 8, weight: .regular)
+        $0.textColor = .subtitleGray
+    }
+    
+    // MARK: - Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Methods
+extension ChatCVC {
+    func initCell(model: ChatModel) {
+        profileImageView.image = model.profileImage
+        userNameLabel.text = model.userName
+        messageLabel.text = model.message
+        timeLabel.text = model.time
+    }
+    
+    private func setUI() {
+        self.backgroundColor = .white
+    }
+    
+    private func setLayout() {
+        addSubviews(profileImageView, userNameLabel, messageLabel, timeLabel)
+        
+        profileImageView.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(16)
+            make.centerY.equalToSuperview()
+            make.width.height.equalTo(40)
+        }
+        
+        userNameLabel.snp.makeConstraints { make in
+            make.leading.equalTo(profileImageView.snp.trailing).offset(10)
+            make.top.equalToSuperview().offset(10)
+        }
+        
+        messageLabel.snp.makeConstraints { make in
+            make.leading.equalTo(profileImageView.snp.trailing).offset(10)
+            make.top.equalTo(userNameLabel.snp.bottom).offset(4)
+        }
+        
+        timeLabel.snp.makeConstraints { make in
+            make.top.equalTo(profileImageView)
+            make.trailing.equalToSuperview().inset(16)
+        }
+    }
+}
