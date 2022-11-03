35ebd09a113e625e27a1b40ec2e8976d547f3978
devxsby <devxsby@gmail.com>
2022-11-03 16:21:19 +0900

    [Add] #5- 이미지 리터럴 추가

--- IN-SOPT-Assignment/IN-SOPT-Assignment/Global/Literals/ImageLiterals.swift
+++ IN-SOPT-Assignment/IN-SOPT-Assignment/Global/Literals/ImageLiterals.swift
@@ -22,6 +22,30 @@ enum ImageLiterals {
     static var profileImg8: UIImage { .load(named: "profileImage8") }
     static var profileImg9: UIImage { .load(named: "profileImage9") }
     
+    static var galleryImage1: UIImage { .load(named: "galleryImage1") }
+    static var galleryImage2: UIImage { .load(named: "galleryImage2") }
+    static var galleryImage3: UIImage { .load(named: "galleryImage3") }
+    static var galleryImage4: UIImage { .load(named: "galleryImage4") }
+    static var galleryImage5: UIImage { .load(named: "galleryImage5") }
+    static var galleryImage6: UIImage { .load(named: "galleryImage6") }
+    static var galleryImage7: UIImage { .load(named: "galleryImage7") }
+    static var galleryImage8: UIImage { .load(named: "galleryImage8") }
+    static var galleryImage9: UIImage { .load(named: "galleryImage9") }
+    static var galleryImage10: UIImage { .load(named: "galleryImage10") }
+    static var galleryImage11: UIImage { .load(named: "galleryImage11") }
+    static var galleryImage12: UIImage { .load(named: "galleryImage12") }
+    static var galleryImage13: UIImage { .load(named: "galleryImage13") }
+    static var galleryImage14: UIImage { .load(named: "galleryImage14") }
+    static var galleryImage15: UIImage { .load(named: "galleryImage15") }
+    static var galleryImage16: UIImage { .load(named: "galleryImage16") }
+    static var galleryImage17: UIImage { .load(named: "galleryImage17") }
+    static var galleryImage18: UIImage { .load(named: "galleryImage18") }
+    static var galleryImage19: UIImage { .load(named: "galleryImage19") }
+    static var galleryImage20: UIImage { .load(named: "galleryImage20") }
+    static var galleryImage21: UIImage { .load(named: "galleryImage21") }
+    static var galleryImage22: UIImage { .load(named: "galleryImage22") }
+    static var galleryImage23: UIImage { .load(named: "galleryImage23") }
+    
     static var settingIcn: UIImage { .load(named: "friendtabSettings") }
     static var closeIcn: UIImage { .load(named: "profileCloseBtn") }
     static var editIcn: UIImage { .load(named: "profileEditImg") }
